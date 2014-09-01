19814d3a51a55d9365bbd436f0b1a0fc73f9fa20
Felipe Tovar <ftovaro@eafit.edu.co>
2014-09-01 17:53:10 -0500

    Left menu with some methods

--- Numerical/gen/co/edu/eafit/numerical/R.java
+++ Numerical/gen/co/edu/eafit/numerical/R.java
@@ -21,10 +21,15 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int drawer_shadow=0x7f020000;
+        public static final int ic_drawer=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080003;
+        public static final int drawer=0x7f080002;
+        public static final int drawer_layout=0x7f080000;
+        public static final int texto=0x7f080001;
     }
     public static final class layout {
         public static final int activity_splash=0x7f030000;
